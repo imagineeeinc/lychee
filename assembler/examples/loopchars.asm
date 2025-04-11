cfea30878d52dc551807d94a68346142bfdaf9e4
imagineeeinc <59028351+imagineeeinc@users.noreply.github.com>
2025-04-11 11:37:01 +0100

    added addition and subtraction

--- assembler/examples/loopchars.asm
+++ assembler/examples/loopchars.asm
@@ -3,11 +3,11 @@ ld h a
 ld a $02
 ld l a
 ld a $07
-ld hl a
+ld (hl) a
 ld a $01
 ld l a
 ld a $19
-ld hl a
+ld (hl) a
 %increment:
 inc (hl)
 jp &increment
--- assembler/src/lychee_asmpkg/main.nim
+++ assembler/src/lychee_asmpkg/main.nim
@@ -18,6 +18,7 @@ proc assemble*(code: string): seq[byte] =
         rom.add(byte 0x3c)
     of "ld":
       var msb: byte = byte 0x00
+      let shift_size: byte = byte 0x08
       var shift: byte = byte 0x00
       var hl: bool = false
       case piece[1].toLowerAscii()
@@ -25,23 +26,24 @@ proc assemble*(code: string): seq[byte] =
         msb = byte 0x40
       of "c":
         msb = byte 0x40
-        shift = 0x07
+        shift = shift_size
       of "d":
         msb = byte 0x50
       of "e":
         msb = byte 0x50
-        shift = 0x07
+        shift = shift_size
       of "h":
         msb = byte 0x60
       of "l":
         msb = byte 0x60
-        shift = 0x07
+        shift = shift_size
       of "(hl)":
         msb = byte 0x70
         hl = true
       of "a":
         msb = byte 0x70
-        shift = 0x07
+        shift = shift_size
+
       case piece[2].toLowerAscii()
       of "b":
         rom.add(bitor[byte](msb, 0x00+shift))
@@ -66,7 +68,54 @@ proc assemble*(code: string): seq[byte] =
           rom.add(fromHex[byte](piece[2][1..^1]))
         else:
           rom.add(bitor[byte](msb, 0x07+shift))
-
+    of "add":
+      case piece[2].toLowerAscii()
+      of "b":
+        rom.add(byte 0x80)
+      of "c":
+        rom.add(byte 0x81)
+      of "d":
+        rom.add(byte 0x82)
+      of "e":
+        rom.add(byte 0x83)
+      of "h":
+        rom.add(byte 0x84)
+      of "l":
+        rom.add(byte 0x85)
+      of "(hl)":
+        rom.add(byte 0x86)
+      of "a":
+        rom.add(byte 0x87)
+      else:
+        if piece[2].startsWith("$"):
+          rom.add(byte 0xc6)
+          rom.add(fromHex[byte](piece[2][1..^1]))
+        else:
+          rom.add(byte 0x87)
+    of "sub":
+      case piece[2].toLowerAscii()
+      of "b":
+        rom.add(byte 0x90)
+      of "c":
+        rom.add(byte 0x91)
+      of "d":
+        rom.add(byte 0x92)
+      of "e":
+        rom.add(byte 0x93)
+      of "h":
+        rom.add(byte 0x94)
+      of "l":
+        rom.add(byte 0x95)
+      of "(hl)":
+        rom.add(byte 0x96)
+      of "a":
+        rom.add(byte 0x97)
+      else:
+        if piece[2].startsWith("$"):
+          rom.add(byte 0xd6)
+          rom.add(fromHex[byte](piece[2][1..^1]))
+        else:
+          rom.add(byte 0x97)
     else:
       discard
   for i in rom:
